1048ad74c13e7da548704ab84ba3fcf63858f217
Jimwell Punzalan <punzalanjimwell@gmail.com>
2025-10-10 20:32:31 +0800

    test7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,16 @@
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+<link rel="stylesheet" href="http://jims-archive.vercel.app/css/app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="images/jlogo.jpg">
     <title>J's Archive</title>
 </head>
 <body>
     <div id="app"></div>
-        <script src="{{asset('js/app.js')}}"></script>
+        <script src="http://jims-archive.vercel.app/js/app.js"></script>
     </body>
 </html>
